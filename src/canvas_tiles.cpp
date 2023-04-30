3372235fa70d5c9b75903175ab89110f642b6bf1
Koen Derks <koen-derks@hotmail.com>
2023-04-30 08:23:52 +0200

    Update canvas_tiles.cpp

--- src/canvas_tiles.cpp
+++ src/canvas_tiles.cpp
@@ -18,10 +18,11 @@
 
 int next_index(const int& resolution,
                const int& index) {
-  if (index < 0)
+  if (index < 0) {
     return index + resolution;
-  if(index >= resolution)
+  } else if (index >= resolution) {
     return index - resolution;
+  }
   return index;
 }
 
@@ -30,24 +31,22 @@ arma::cube iterate_tile(const arma::cube& canvas,
                         const double& diffa,
                         const double& diffb,
                         const double& feedrate,
-                        const double& killrate){
+                        const double& killrate) {
   const int resolution = canvas.n_rows;
+  const double kplusf = killrate + feedrate;
   arma::cube new_canvas(resolution, resolution, 2);
-  for(int x = 0; x < resolution; ++x) {
-    Rcpp::checkUserInterrupt();
-    for(int y = 0; y < resolution; ++y){
-      double asum = 0.0;
-      double bsum = 0.0;
-      for(int i = -1; i <= 1; ++i){
-        for(int j = -1; j <= 1; ++j){
-          const int x1 = next_index(resolution, x - i);
-          const int y1 = next_index(resolution, y - j);
-          asum += conv.at(i + 1, j + 1) * canvas.at(x1, y1, 0);
-          bsum += conv.at(i + 1, j + 1) * canvas.at(x1, y1, 1);
+  for (int x = 0; x < resolution; ++x) {
+    for (int y = 0; y < resolution; ++y) {
+      double asum = 0, bsum = 0;
+      for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+          const int xn = next_index(resolution, x - i);
+          const int yn = next_index(resolution, y - j);
+          asum += conv.at(i + 1, j + 1) * canvas.at(xn, yn, 0);
+          bsum += conv.at(i + 1, j + 1) * canvas.at(xn, yn, 1);
         }
       }
       const double xpow = pow(canvas.at(x, y, 1), 2);
-      const double kplusf = killrate + feedrate;
       new_canvas.at(x, y, 0) = canvas.at(x, y, 0) + diffa * asum - canvas.at(x, y, 0) * xpow + feedrate * (1 - canvas.at(x, y, 0));
       new_canvas.at(x, y, 1) = canvas.at(x, y, 1) + diffb * bsum + canvas.at(x, y, 0) * xpow - kplusf * canvas.at(x, y, 1);
     }
@@ -62,8 +61,9 @@ arma::cube draw_tile(arma::cube& canvas,
                      const double& diffb,
                      const double& feedrate,
                      const double& killrate,
-                     const int& iterations){
-  for(int i = 0; i < iterations; ++i){
+                     const int& iterations) {
+  for (int i = 0; i < iterations; ++i) {
+    Rcpp::checkUserInterrupt();
     canvas = iterate_tile(canvas, conv, diffa, diffb, feedrate, killrate); 
   }
   return canvas;
